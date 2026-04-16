e7fa58e6298e33e46e930937d1f9a2a86ad175a8
matheusburey <matheusbureidev@gmail.com>
2026-04-16 18:15:38 -0300

    refactor: migrate project to bun

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+	// NOTE: Update this to include the paths to all files that contain Nativewind classes.
+	content: ["./src/**/*.tsx"],
+	presets: [require("nativewind/preset")],
+	theme: {
+		extend: {
+			fontFamily: {
+				title: ["Inter_700Bold"],
+				body: ["Inter_400Regular"],
+			},
+			colors: {
+				purple: {
+					500: "#8615DF",
+					600: "#570E91",
+					800: "#38085C",
+					900: "#190429",
+				},
+				gray: {
+					50: "#F6F6F7",
+					100: "#EEEEEE",
+					200: "#D4D4D4",
+					300: "#9E9EA7",
+					400: "#666665",
+					900: "#111111",
+				},
+				red: {
+					600: "#DF1545",
+				},
+				green: {
+					600: "#168821",
+				},
+			},
+		},
+	},
+	plugins: [],
+};
