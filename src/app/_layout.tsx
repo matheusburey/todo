21c95f5eba5bf77905af3509f10964eb4d81c18a
matheusburey <matheusbureidev@gmail.com>
2026-04-16 18:19:04 -0300

    refactor: change project to react native

---
+++ src/app/_layout.tsx
@@ -0,0 +1,46 @@
+import {
+	Inter_400Regular,
+	Inter_700Bold,
+	useFonts,
+} from "@expo-google-fonts/inter";
+import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
+
+import "../assets/global.css";
+
+SplashScreen.preventAutoHideAsync();
+
+SplashScreen.setOptions({
+	fade: true,
+	duration: 500,
+});
+
+export default function RootLayout() {
+	const [loaded, error] = useFonts({
+		Inter_400Regular,
+		Inter_700Bold,
+	});
+
+	useEffect(() => {
+		if (loaded || error) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded, error]);
+
+	if (!loaded && !error) {
+		return null;
+	}
+
+	return (
+		<>
+			<Stack
+				screenOptions={{
+					headerShown: false,
+				}}
+			/>
+			<StatusBar style="light" animated />
+		</>
+	);
+}
--- src/routes/PrivateRoute.tsx
+++
@@ -1,8 +0,0 @@
-import { Outlet, Navigate } from "react-router-dom";
-
-import { useAuth } from "../providers/Auth";
-
-export function PrivateRoute() {
-  const { user } = useAuth();
-  return user?.acessToken ? <Outlet /> : <Navigate to="/" />;
-}
