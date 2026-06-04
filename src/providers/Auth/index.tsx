4d02128468eab319c34c5dc36ba98016d3aabc27
matheusburey <matheusbureidev@gmail.com>
2026-06-04 11:24:21 -0300

    refactor(auth): update auth provider and introduce useAuth hook

--- src/providers/Auth/index.tsx
+++ src/providers/Auth/index.tsx
@@ -1,35 +1,41 @@
-import { useToast } from "@chakra-ui/react";
-import { createContext, useContext, useCallback, useState, useMemo } from "react";
+import {
+	createContext,
+	type PropsWithChildren,
+	useCallback,
+	useMemo,
+	useState,
+} from "react";
 
 import { sigInApi } from "../../api/user";
-import { IDataSignIn } from "../../api/user/types";
-import { IProps, IAuthContext, IUser } from "./types";
+import type { IDataSignIn } from "../../api/user/types";
+import type { IAuthContext, IUser } from "./types";
 
-const AuthContext = createContext({} as IAuthContext);
+export const AuthContext = createContext({} as IAuthContext);
 
-export const useAuth = () => useContext(AuthContext);
+export function AuthProvider({ children }: PropsWithChildren) {
+	const [user, setUser] = useState<IUser>(
+		JSON.parse(localStorage.getItem("@Doit:user") || "{}") as IUser,
+	);
 
-export function AuthProvider({ children }: IProps) {
-  const toast = useToast();
-  const [user, setUser] = useState<IUser>(JSON.parse(localStorage.getItem("@Doit:user") || "{}") as IUser);
+	const signIn = useCallback(async (data: IDataSignIn) => {
+		const res = await sigInApi(data);
+		if (res?.detail.status === "ok") {
+			localStorage.setItem("@Doit:user", JSON.stringify(res.data));
+			setUser(res.data as IUser);
+			return true;
+		}
+		return false;
+	}, []);
 
-  const signIn = useCallback(async (data: IDataSignIn) => {
-    const res = await sigInApi(data);
-    if (res?.detail.status === "ok") {
-      localStorage.setItem("@Doit:user", JSON.stringify(res.data));
-      setUser(res.data as IUser);
-      return true;
-    }
-    toast({ position: "top-right", title: res.detail.description, status: "error" });
-    return false;
-  }, []);
+	const signOut = useCallback(async () => {
+		localStorage.removeItem("@Doit:user");
+		setUser({} as IUser);
+	}, []);
 
-  const signOut = async () => {
-    localStorage.removeItem("@Doit:user");
-    setUser({} as IUser);
-  };
+	const value = useMemo(
+		() => ({ user, signIn, signOut }),
+		[user, signIn, signOut],
+	);
 
-  const value = useMemo(() => ({ user, signIn, signOut }), [user]);
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+	return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 }
--- src/providers/Auth/types.d.ts
+++ src/providers/Auth/types.d.ts
@@ -1,18 +1,12 @@
-import { ReactNode } from "react";
-
-export interface IProps {
-  children: ReactNode;
-}
-
 export interface IAuthContext {
-  user: IUser;
-  signIn: (data: IDataSignIn) => Promise<boolean>;
-  signOut: (data: IDataSignIn) => Promise<void>;
+	user: IUser;
+	signIn: (data: IDataSignIn) => Promise<boolean>;
+	signOut: (data: IDataSignIn) => Promise<void>;
 }
 
 export interface IUser {
-  id: number;
-  email: string;
-  acessToken: string;
-  name: string;
+	id: number;
+	email: string;
+	accessToken: string;
+	name: string;
 }
